6129a833179dbd0288080568ff5eb06db910432c
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-06-21 21:16:54 +0100

    Create tailwind.config.js

---
+++ App/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // optional, can be 'media' if you prefer
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--color-background)",
+        foreground: "var(--color-foreground)",
+        primary: "var(--color-primary)",
+      },
+    },
+  },
+  plugins: [],
+};
